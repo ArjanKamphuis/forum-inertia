8f6c921565dc7c316b0a52a3b1dc778c107265cb
Arjan Kamphuis <arjan_kamphuis@hotmail.com>
2023-02-26 22:54:17 +0100

    001 - Initial Database Setup With Seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,23 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Reply;
+use App\Models\Thread;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $threads = Thread::factory(50)->create();
+        $users = User::all();
+        $threads->each(function(Thread $thread) use ($users) {
+            Reply::factory(10)->create([
+                'thread_id' => $thread,
+                'user_id' => $users->random()
+            ]);
+        });
     }
 }
