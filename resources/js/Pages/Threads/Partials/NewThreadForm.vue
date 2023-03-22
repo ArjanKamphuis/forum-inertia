02c3b1f57a5faf800fbac3896d36eea877aac18f
Arjan Kamphuis <arjan_kamphuis@hotmail.com>
2023-03-22 14:41:35 +0100

    012 - Validation Errors and Old Data

--- resources/js/Pages/Threads/Partials/NewThreadForm.vue
+++ resources/js/Pages/Threads/Partials/NewThreadForm.vue
@@ -1,13 +1,16 @@
 <script setup>
-import { useForm } from '@inertiajs/vue3';
+import { useForm, usePage } from '@inertiajs/vue3';
+
 import Card from '@/Components/Card.vue';
 import InputError from '@/Components/InputError.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextArea from '@/Components/TextArea.vue';
 import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import SelectInput from '@/Components/SelectInput.vue';
+import TextArea from '@/Components/TextArea.vue';
 import TextInput from '@/Components/TextInput.vue';
 
 const form = useForm({
+    channel_id: '',
     title: '',
     body: ''
 });
@@ -18,13 +21,21 @@ const form = useForm({
         <template #body>
             <form @submit.prevent="form.post('/threads')" id="new-thread-form" class="space-y-4">
                 <div>
-                    <InputLabel for="title" value="Title" />
-                    <TextInput v-model="form.title" id="title" type="text" class="block w-full" />
+                    <InputLabel for="channel_id" value="Choose a channel:" />
+                    <SelectInput v-model.number="form.channel_id" id="channel_id" class="block w-full" required>
+                        <option disabled value="">Choose One...</option>
+                        <option v-for="channel in usePage().props.channels" :key="channel.id" :value="channel.id" v-text="channel.name" />
+                    </SelectInput>
+                    <InputError :message="form.errors.channel_id" class="mt-2" />
+                </div>
+                <div>
+                    <InputLabel for="title" value="Title:" />
+                    <TextInput v-model="form.title" id="title" type="text" class="block w-full" required />
                     <InputError :message="form.errors.title" class="mt-2" />
                 </div>
                 <div>
-                    <InputLabel for="body" value="Body" />
-                    <TextArea v-model="form.body" id="body" class="block w-full h-48" />
+                    <InputLabel for="body" value="Body:" />
+                    <TextArea v-model="form.body" id="body" class="block w-full h-48" required />
                     <InputError :message="form.errors.body" class="mt-2" />
                 </div>
             </form>
