14715baf18296ed557f1e68297d708906f30fa31
Arjan Kamphuis <arjan_kamphuis@hotmail.com>
2023-04-01 21:03:05 +0200

    Async Card on index

--- resources/js/Pages/Threads/Index.vue
+++ resources/js/Pages/Threads/Index.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { computed } from 'vue';
+import { computed, defineAsyncComponent } from 'vue';
 import { Head, Link } from '@inertiajs/vue3';
-import Card from '@/Components/Card.vue';
 import DefaultLayout from '@/Layouts/DefaultLayout.vue';
 
-const props = defineProps({ threads: Object });
+const Card = defineAsyncComponent(() => import('@/Components/Card.vue'));
+
+const props = defineProps({ threads: Array });
 const title = computed(() => 'Forum Threads');
 </script>
 
@@ -15,7 +16,7 @@ const title = computed(() => 'Forum Threads');
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">{{ title }}</h2>
         </template>
         <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-            <div class="space-y-4">
+            <div v-if="threads.length" class="space-y-4">
                 <Card v-for="thread in threads" :key="thread.id">
                     <template #header>
                         <div class="flex justify-between items-center">
@@ -30,6 +31,7 @@ const title = computed(() => 'Forum Threads');
                     </template>
                 </Card>
             </div>
+            <div v-else class="text-center">There are no relevant results at this time. &#128064;</div>
         </div>
     </DefaultLayout>
 </template>
